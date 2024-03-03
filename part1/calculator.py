73ae94398d55c383e5a62c5bc49ab255bc1dbb76
Nupur Singh <nupur.singh2410@gmail.com>
2024-03-03 12:14:45 +0530

    First Commit

---
+++ part1/calculator.py
@@ -0,0 +1,9 @@
+INTEGER, PLUS, EOF= 'INTEGER', 'PLUS', 'EOF'
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type= type
+        self.value= value
+    
+    def __str__(self):
+        return f'Token({self.type}, {self.value})'
